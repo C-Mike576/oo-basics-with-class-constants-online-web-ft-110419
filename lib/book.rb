5b9507d136d39fb5dd6e76eef6498b34753b3489
Michael Calkins <m.calkins576@gmail.com>
2019-11-06 21:14:51 +0000

    Automatically backed up by Learn

--- lib/book.rb
+++ lib/book.rb
@@ -1,11 +1,18 @@
 class Book
-  attr_accessor :author, :page_count, :genre
+  attr_accessor :author, :page_count
   attr_reader :title
-
+  
+  GENRE = []
+  
   def initialize(title)
     @title = title
   end
-
+  
+  def genre=(genre)
+    @genre = genre
+    GENRE << genre
+  end
+  
   def turn_page
     puts "Flipping the page...wow, you read fast!"
   end
